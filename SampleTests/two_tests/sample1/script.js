8e441a2dcf213cb810e9f538093b253fd5122de4
NicoNL <a0t4zr@inf.elte.hu>
2024-03-03 11:26:37 +0100

    Code optimization

--- SampleTests/two_tests/sample1/script.js
+++ SampleTests/two_tests/sample1/script.js
@@ -3,7 +3,7 @@ let grades = [22, 13, 26, 27, 28, 30, 30, 25, 17, 18, 21, 21, 25, 26]
 //First Point
 let gradesA = grades.filter(grade => grade >= 24);
 let f1_1 = document.querySelector("#f1-1");
-f1_1.innerHTML = f1_1.innerHTML + "The grades with five are " + gradesA.length;
+f1_1.innerHTML += "The grades with five are " + gradesA.length;
 
 //Second Point
 let sumOfGrades = 0;
@@ -11,16 +11,16 @@ grades.forEach(grade => {
     sumOfGrades += parseInt(grade);
 });
 let f1_2 = document.querySelector("#f1-2");
-f1_2.innerHTML = f1_2.innerHTML + "The average of the class is " +(sumOfGrades/grades.length);
+f1_2.innerHTML += "The average of the class is " +(sumOfGrades/grades.length);
 
 // Third Point
 let failGrade = grades.every(grade => grade > 10);
 let f1_3 = document.querySelector("#f1-3");
 if(failGrade){
-    f1_3.innerHTML = f1_3.innerHTML + " Yes, there was."
+    f1_3.innerHTML += " Yes, there was."
 }
 else{
-    f1_3.innerHTML = f1_3.innerHTML + " No, there wasn't."    
+    f1_3.innerHTML += " No, there wasn't."    
 }
 
 //fourth Point
